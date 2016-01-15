debe7482440c668afa54426508cd4041c3c15254
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-01-15 03:42:26 -0500

    revert move to PAIR

--- remotedocker/client.py
+++ remotedocker/client.py
@@ -11,64 +11,62 @@ import requests
 import click
 from zmq import ssh
 
-# HOST = 'lheinric-dockerinteractive'
-# WEBSERVERHOST = 'localhost:3000'
-# TUNNEL = True
-
-HOST = 'localhost'
-WEBSERVERHOST = 'localhost:5000'
-TUNNEL = False
-
-
 @click.command()
 @click.argument('container')
 @click.argument('command')
 def client(container,command):
     context = zmq.Context()
 
-    r = requests.get('http://{}/start?container={}&command={}'.format(WEBSERVERHOST,container,command))
-    readfrom = r.json()['readfrom']
+    r = requests.get('http://localhost:3000/start?container={}&command={}'.format(container,command))
+    readfrom, writeto = r.json()['readfrom'],r.json()['writeto']
 
     click.secho('starting remote docker session', fg = 'green')
 
     #incoming messages
-    socket = context.socket(zmq.PAIR)
-    if TUNNEL:
-        ssh.tunnel_connection(socket,'tcp://{}:{}'.format(HOST,readfrom),'lxplus')
-    else:
-        socket.connect('tcp://{}:{}'.format(HOST,readfrom))
+    sub_socket = context.socket(zmq.SUB)
+    # sub_socket.("tcp://localhost:{}".format(readfrom))
+    ssh.tunnel_connection(sub_socket,'tcp://lheinric-dockerinteractive:{}'.format(readfrom),'lxplus')
+    sub_socket.setsockopt(zmq.SUBSCRIBE,'')
+
+    #outgoing messages
+    pub_socket = context.socket(zmq.REQ)
+    # pub_socket.connect("tcp://localhost:{}".format(writeto))
+    ssh.tunnel_connection(pub_socket,'tcp://lheinric-dockerinteractive:{}'.format(writeto),'lxplus')
 
     poller = zmq.Poller()
-    poller.register(socket,zmq.POLLIN)
-    sockets = [socket]
+    poller.register(sub_socket,zmq.POLLIN)
+    poller.register(pub_socket,zmq.POLLIN)
+    sockets = [pub_socket,sub_socket]
 
-    socket.send('start')
-    ack = socket.recv()
+    pub_socket.send('start')
+    ack = pub_socket.recv()
 
 
     click.secho('we\'ll be with you shortly...', fg = 'green')
-    m = socket.recv()
+    m = sub_socket.recv()
 
     oldtty = termios.tcgetattr(sys.stdin)
 
     try:
+        tty.setraw(sys.stdin.fileno())
         tty.setcbreak(sys.stdin.fileno())
         while True:
             r, w, x  = select.select([sys.stdin], [], [], 0.0)
-            zr,zw,zx = zmq.select([socket],[socket],[], timeout = 0.0)
+            zr,zw,zx = zmq.select([sub_socket],[pub_socket],[], timeout = 0.0)
 
-            if (sys.stdin in r) and (socket in zw):
+            if (sys.stdin in r) and (pub_socket in zw):
                 x = sys.stdin.read(1)
-                socket.send(x)
-                socket.recv()
+                pub_socket.send(x)
+                pub_socket.recv()
         
-            if (socket in zr):
-                x = socket.recv()
+            if (sub_socket in zr):
+                x = sub_socket.recv()
                 if len(x) == 0:
                     sys.stdout.write('\r\nexiting... \r\n')
                     break
                 sys.stdout.write(x)
                 sys.stdout.flush()
+            # time.sleep(0.0001)
     finally:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
 
--- remotedocker/runserver.py
+++ remotedocker/runserver.py
@@ -9,8 +9,8 @@ def set_winsize(fd, row, col, xpix=0, ypix=0):
 
 
 
-def start_server(publishport, container, command):
-    print "enter function {} {} {}".format(publishport, container, command)
+def start_server(publishport, subscribeport, container, command):
+    print "enter function {} {} {} {}".format(publishport, subscribeport, container, command)
     import time
     import zmq
     import subprocess
@@ -21,12 +21,24 @@ def start_server(publishport, container, command):
     context = zmq.Context()
 
     #outgoing messages
-    socket = context.socket(zmq.PAIR)
-    socket.bind("tcp://0.0.0.0:{}".format(publishport))
+    pub_socket = context.socket(zmq.PUB)
+    pub_socket.bind("tcp://0.0.0.0:{}".format(publishport))
+
+
+    #incoming messages
+    #we will just reply every incoming message (request) with 'ack!'
+    rep_socket = context.socket(zmq.REP)
+    rep_socket.bind("tcp://0.0.0.0:{}".format(subscribeport))
+
+    #incoming messages
+    #sub_socket = context.socket(zmq.SUB)
+    #sub_socket.connect("tcp://0.0.0.0:{}".format(subscribeport))
+    #sub_socket.setsockopt(zmq.SUBSCRIBE,'')
 
     poller = zmq.Poller()
-    poller.register(socket,zmq.POLLIN)
-    sockets = [socket]
+    poller.register(pub_socket,zmq.POLLIN)
+    poller.register(rep_socket,zmq.POLLIN)
+    sockets = [rep_socket,pub_socket]
 
     import pty
     master, slave = pty.openpty()
@@ -36,24 +48,32 @@ def start_server(publishport, container, command):
     afsdirmount = '/afs/cern.ch/user/l/lheinric/testafs'
     subprocess.call(shlex.split('cvmfs_config probe'))
     print "start docker"
-    # p = subprocess.Popen(shlex.split('docker run -it -v /cvmfs:/cvmfs -v {}:/output {} {}'.format(afsdirmount,container,command)), stdin = slave, stdout = slave, stderr = slave)
-    p = subprocess.Popen(shlex.split('sh -i'), stdin = slave, stdout = slave, stderr = slave)
+    p = subprocess.Popen(shlex.split('docker run -it -v /cvmfs:/cvmfs -v {}:/output {} {}'.format(afsdirmount,container,command)), stdin = slave, stdout = slave, stderr = slave)
+    #p = subprocess.Popen(shlex.split('sh -i'), stdin = slave, stdout = slave, stderr = slave)
 
     import os
     import select
 
+
     print "starting server!"
+
+
     print "wait for start signal from client"
-    start = socket.recv()
+    start = rep_socket.recv()
 
     print "acknowledge!"
-    socket.send('ack '.format(start))
+    rep_socket.send('ack '.format(start))
 
+    time.sleep(5)
     print "first publish"
-    socket.send('hi there... ')
+    pub_socket.send('hi there... ')
 
-    sockets = [socket]
+    #print "wait for stop signal from client"
+    #stop = rep_socket.recv()
+    #print "acknowledge!"
+    #rep_socket.send('ack '.format(stop)
 
+    sockets = [pub_socket,rep_socket]
     while True:
         #print "polling"
         r, w, x = select.select([master],[master],[master], 0.0)
@@ -68,23 +88,27 @@ def start_server(publishport, container, command):
 
         if procpoll is not None:
 	    print "ending session because process ended"
-            socket.send('')
+            pub_socket.send('')
             return
         
-        if (master in r) and (socket in zw):
+    
+        if (master in r) and (pub_socket in zw):
             #print "reading!"
             fromprocess = os.read(master,1024)
             #print 'sending {}'.format(fromprocess)
-            socket.send(fromprocess)
+            pub_socket.send(fromprocess)
 
-        if (master in w) and (socket in zr):
+        if (master in w) and (rep_socket in zr):
             #  Wait for next request from client
             #print "recv"
-            message = socket.recv()
-            socket.send('')
+            message = rep_socket.recv()
+            rep_socket.send('')
             #print("Received request: %s" % repr(message))
             os.write(master,message)
             #print "wrote it"
+
+    
+#        time.sleep(0.001)
 
 if __name__ == '__main__':
     start_server(5556, 5557, 'container', 'command')
